b0f35ca99439705bcb1e87aca5b7b81cbdbaf5a7
Richard Frank <richardfrank@me.com>
2015-12-12 22:19:56 -0600

    Moar Trees

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,10 @@
 var game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image('tree', 'assets/bark-1-blur.png');
+  game.load.image('tree', 'assets/tree-01.png');
+  game.load.image('tree', 'assets/tree-02.png');
+  game.load.image('tree', 'assets/tree-bite-01.png');
+  game.load.image('tree', 'assets/tree-bite-02.png');
 }
 
 function create() {
